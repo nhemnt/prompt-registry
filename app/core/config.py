8bae7043413f53e55cbe77ab96d5d19982c13295
Hemant Negi <hemantnegi@Hemants-MacBook-Air.local>
2026-02-14 22:55:29 +0530

    Add SQLAlchemy + asyncpg

--- app/core/config.py
+++ app/core/config.py
@@ -3,6 +3,7 @@ from pydantic_settings import BaseSettings
 class Settings(BaseSettings):
     app_name: str = 'Prompt Registry'
     debug: bool = True
+    database_url: str
     
     class Config:
         env_file = '.env'
