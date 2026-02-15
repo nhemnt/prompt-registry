fe2953bccceaba0c6f5f402dc8cd21c7ec8f1341
Hemant Negi <nhemnt@gmail.com>
2026-02-15 10:55:01 +0530

    Create Base model class (#4)
    Co-authored-by: Hemant Negi <hemantnegi@Hemants-MacBook-Air.local>

--- app/db/session.py
+++ app/db/session.py
@@ -1,24 +1,23 @@
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine, async_sessionmaker
 from sqlalchemy.orm import DeclarativeBase
+from typing import AsyncGenerator
+
 from app.core.config import settings
-
-
-class Base(DeclarativeBase):
-    pass
-
+from app.db.base import Base
 
 engine = create_async_engine(
     settings.database_url,
     echo=True,  # set False later in production
 )
 
+# Create session factory
 AsyncSessionLocal = async_sessionmaker(
     engine,
     expire_on_commit=False,
     class_=AsyncSession,
 )
 
-async def get_db():
+async def get_db() -> AsyncGenerator[AsyncSession, None]:
     async with AsyncSessionLocal() as session:
         yield session
 
